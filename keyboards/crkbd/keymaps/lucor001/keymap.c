f6199e6a1e1369294aee99340559a980424ada5e
lucor001 <163668327+lucor001@users.noreply.github.com>
2025-04-08 21:16:35 -0500

    fixed errors in keymap

--- keyboards/crkbd/keymaps/lucor001/keymap.c
+++ keyboards/crkbd/keymaps/lucor001/keymap.c
@@ -155,29 +155,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC, TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_EXTRA), TD(U_TD_U_BASE), KC_NO, QK_LOCK, QK_LOCK, KC_LBRC,    KC_7, KC_8, KC_9, KC_RBRC, KC_BSPC,
    KC_TAB,         CKC_A,          CKC_R,            CKC_S,           CKC_T, KC_NO, QK_LLCK, QK_LLCK, KC_COLN,    KC_4, KC_5, KC_6,  KC_EQL, KC_QUOT,
 //   KC_TAB,  LGUI_T(KC_A),   LALT_T(KC_R),     LCTL_T(KC_S),    LSFT_T(KC_T), KC_NO, QK_LLCK, QK_LLCK, KC_COLN,    KC_4, KC_5, KC_6,  KC_EQL, KC_QUOT,
-  KC_LSFT,         KC_NO,        KC_ALGR,   TD(U_TD_U_FUN),  TD(U_TD_U_NUM), KC_NO,                    KC_GRV,    KC_1, KC_2, KC_3, KC_BSLS, KC_RSFT,
-                                                                      KC_NO, KC_NO,   KC_NO, KC_DOT,     KC_0, KC_MINS 
+  KC_LSFT,         KC_NO,        KC_ALGR, TD(U_TD_U_MOUSE),  TD(U_TD_U_NUM), KC_NO,                    KC_GRV,    KC_1, KC_2, KC_3, KC_BSLS, KC_RSFT,
+                                                                      KC_NO, KC_NO,   KC_NO,  KC_DOT,    KC_0, KC_MINS 
   ),
   [U_SYM] = LAYOUT_split_3x6_3_ex2(
    KC_ESC, TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_EXTRA), TD(U_TD_U_BASE), KC_NO, QK_LOCK, QK_LOCK, KC_LBRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
    KC_TAB,         CKC_A,          CKC_R,            CKC_S,           CKC_T, KC_NO, QK_LLCK, QK_LLCK, KC_SCLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, KC_QUOT,
 //   KC_TAB,  LGUI_T(KC_A),   LALT_T(KC_R),     LCTL_T(KC_S),    LSFT_T(KC_T), KC_NO, QK_LLCK, QK_LLCK, KC_SCLN,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, KC_QUOT,
-  KC_LSFT,         KC_NO,        KC_ALGR, TD(U_TD_U_MOUSE),  TD(U_TD_U_SYM), KC_NO,                   KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, KC_RSFT,
+  KC_LSFT,         KC_NO,        KC_ALGR,   TD(U_TD_U_FUN),  TD(U_TD_U_SYM), KC_NO,                   KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, KC_RSFT,
                                                                       KC_NO, KC_NO,   KC_NO, KC_LPRN, KC_RPRN, KC_UNDS 
   ),
   [U_FUN] = LAYOUT_split_3x6_3_ex2(
    KC_ESC, KC_F12, KC_F7, KC_F8,  KC_F9, KC_PSCR, QK_LOCK, QK_LOCK, TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_EXTRA), TD(U_TD_U_BASE),   KC_NO, KC_BSPC,
    KC_TAB, KC_F11, KC_F4, KC_F5,  KC_F6, KC_SCRL, QK_LLCK, QK_LLCK,         CKC_N,          CKC_E,            CKC_I,           CKC_O,   KC_NO, KC_QUOT,
 //   KC_TAB, KC_F11, KC_F4, KC_F5,  KC_F6, KC_SCRL, QK_LLCK, QK_LLCK,  LGUI_T(KC_A),   LALT_T(KC_R),     LCTL_T(KC_S),    LSFT_T(KC_T),   KC_NO, KC_QUOT,
-  KC_LSFT, KC_F10, KC_F1, KC_F2,  KC_F3, KC_PAUS,                           KC_NO,        KC_ALGR,   TD(U_TD_U_NUM),  TD(U_TD_U_FUN),   KC_NO, KC_RSFT,
-                                  KC_NO,   KC_NO,   KC_NO,  KC_APP,        KC_SPC,         KC_TAB 
+  KC_LSFT, KC_F10, KC_F1, KC_F2,  KC_F3, KC_PAUS,                           KC_NO,        KC_ALGR,   TD(U_TD_U_FUN),  TD(U_TD_U_SYM),   KC_NO, KC_RSFT,
+                                 KC_APP,  KC_SPC,  KC_TAB,   KC_NO,         KC_NO,          KC_NO
   ),
   [U_MOUSE] = LAYOUT_split_3x6_3_ex2(
    KC_ESC, TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   KC_NO, QK_LOCK, QK_LOCK,   U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND, KC_BSPC,
    KC_TAB,         CKC_A,          CKC_R,            CKC_S,            CKC_T,   KC_NO, QK_LLCK, QK_LLCK,   KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_QUOT,
 //   KC_TAB,  LGUI_T(KC_A),   LALT_T(KC_R),     LCTL_T(KC_S),     LSFT_T(KC_T),   KC_NO, QK_LLCK, QK_LLCK,   KC_NO, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_QUOT,
-  KC_LSFT,         KC_NO,        KC_ALGR,   TD(U_TD_U_SYM), TD(U_TD_U_MOUSE),   KC_NO,                     KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_RSFT,
-                                                                     KC_BTN3, KC_BTN1, KC_BTN2, KC_BTN2, KC_BTN1, KC_BTN3 
+  KC_LSFT,         KC_NO,        KC_ALGR,   TD(U_TD_U_NUM), TD(U_TD_U_MOUSE),   KC_NO,                     KC_NO, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_RSFT,
+                                                                     KC_BTN3, KC_BTN1, KC_BTN2,   KC_NO,   KC_NO,   KC_NO 
   ),
   [U_BUTTON] = LAYOUT_split_3x6_3_ex2(
    KC_ESC, U_UND, U_CUT, U_CPY,   U_PST,   U_RDO, QK_LOCK, QK_LOCK,   U_RDO,   U_PST, U_CPY, U_CUT, U_UND, KC_BSPC,
@@ -190,7 +190,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC, TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_EXTRA), TD(U_TD_U_BASE), KC_NO, QK_LOCK, QK_LOCK,   U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND, KC_BSPC,
    KC_TAB,         CKC_A,          CKC_R,            CKC_S,           CKC_T, KC_NO, QK_LLCK, QK_LLCK, CW_TOGG, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT,
 //   KC_TAB,  LGUI_T(KC_A),   LALT_T(KC_R),     LCTL_T(KC_S),    LSFT_T(KC_T), KC_NO, QK_LLCK, QK_LLCK, CW_TOGG, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_QUOT,
-  KC_LSFT,       KC_ALGR, TD(U_TD_U_NUM),   TD(U_TD_U_NAV),           KC_NO, KC_NO,                    KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_RSFT,
+  KC_LSFT,         KC_NO,        KC_ALGR, TD(U_TD_U_MEDIA),  TD(U_TD_U_NAV), KC_NO,                    KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, KC_RSFT,
                                                                       KC_NO, KC_NO,   KC_NO,  KC_ENT, KC_BSPC,  KC_DEL 
   ),
   [U_EXTRA] = LAYOUT_split_3x6_3_ex2(
@@ -204,8 +204,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    KC_ESC, TD(U_TD_BOOT), TD(U_TD_U_TAP), TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   KC_NO, QK_LOCK, QK_LOCK, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_BSPC,
    KC_TAB,         CKC_A,          CKC_R,            CKC_S,            CKC_T,   KC_NO, QK_LLCK, QK_LLCK,   KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_QUOT,
 //   KC_TAB,  LGUI_T(KC_A),   LALT_T(KC_R),     LCTL_T(KC_S),     LSFT_T(KC_T),   KC_NO, QK_LLCK, QK_LLCK,   KC_NO, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_QUOT,
-  KC_LSFT,         KC_NO,        KC_ALGR,   TD(U_TD_U_FUN), TD(U_TD_U_MEDIA),   KC_NO,                   OU_AUTO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_RSFT,
-                                                                       KC_NO,   KC_NO,   KC_NO, KC_MSTP, KC_MPLY, KC_MUTE 
+  KC_LSFT,         KC_NO,        KC_ALGR,   TD(U_TD_U_NAV), TD(U_TD_U_MEDIA),   KC_NO,                   OU_AUTO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_RSFT,
+                                                                     KC_MSTP, KC_MPLY, KC_MUTE,   KC_NO,   KC_NO,   KC_NO
   ),
   [U_TAP] = LAYOUT_split_3x6_3_ex2(
    KC_ESC, KC_Q, KC_W, KC_F,   KC_P,   KC_B,  TD(U_TD_U_BASE), TD(U_TD_BOOT),    KC_J,   KC_L,    KC_U,   KC_Y, KC_SCLN, KC_BSPC,
